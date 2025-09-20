d70b19b8b013749a1abe6c981a4adab8b6b90a18
Vicmalash <malangavic@yahoo.com>
2025-09-20 19:50:16 +0300

    Edited Test

--- models/images.js
+++ models/images.js
@@ -1,23 +1,24 @@
 const express = require('express');
 const router = express.Router();
-
 const Image = process.env.NODE_ENV === 'test'
-  ? {
-      find: async () => [
-        { _id: '1', name: 'Test Image 1', path: '/test1.jpg', size: 123 },
-        { _id: '2', name: 'Test Image 2', path: '/test2.jpg', size: 456 },
-      ]
-    }
+  ? { find: async () => [
+      { _id: '1', name: 'Test Image 1', path: '/test1.jpg', size: 123 },
+      { _id: '2', name: 'Test Image 2', path: '/test2.jpg', size: 456 },
+    ] }
   : require('../models/images');
 
-// Return all images as JSON
+// GET /image - returns JSON array of images in test mode, otherwise normal behavior
 router.get('/', async (req, res) => {
   try {
-    const images = await Image.find();
-    res.json(images);
+    const images = await Image.find({});
+    if (process.env.NODE_ENV === 'test') {
+      return res.json(images); // return JSON for tests
+    } else {
+      return res.render('image', { images });
+    }
   } catch (err) {
     console.error(err);
-    res.status(500).json({ error: 'Server Error' });
+    res.status(500).send("Server Error");
   }
 });
 
