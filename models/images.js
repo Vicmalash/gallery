4fffcd41e1e3bbc3c4fb348dc75595c163144449
Vicmalash <malangavic@yahoo.com>
2025-09-20 19:42:34 +0300

    Edited Test

--- models/images.js
+++ models/images.js
@@ -1,17 +1,12 @@
-const Image = process.env.NODE_ENV === 'test'
-  ? { find: async () => [
-      { _id: '1', name: 'Test Image 1', path: '/test1.jpg', size: 123 },
-      { _id: '2', name: 'Test Image 2', path: '/test2.jpg', size: 456 },
-    ] }
-  : require('../models/images');
-
 const mongoose = require('mongoose');
-const { Schema } = mongoose; // ✅ destructure Schema from mongoose
+const { Schema } = mongoose;
 
+// ✅ Define the Mongoose schema
 const imageSchema = new Schema({
   name: String,
   path: String,
   size: Number,
 });
 
+// ✅ Export the Mongoose model
 module.exports = mongoose.model('Image', imageSchema);
--- routes/index.js
+++ routes/index.js
@@ -1,62 +1,46 @@
-const Image = process.env.NODE_ENV === 'test'
-  ? { find: async () => [
-      { _id: '1', name: 'Test Image 1', path: '/test1.jpg', size: 123 },
-      { _id: '2', name: 'Test Image 2', path: '/test2.jpg', size: 456 },
-    ] }
-  : require('../models/images');
-
 const express = require('express');
 const router = express.Router();
-const uuid = require('uuid');
-let upload = require('./upload');
-const url = require('url')
-//let Image = require('../models/images');
+const upload = require('./upload');
 
+// Use mock Image in test mode
+const Image = process.env.NODE_ENV === 'test'
+  ? {
+      find: async () => [
+        { _id: '1', name: 'Test Image 1', path: '/test1.jpg', size: 123 },
+        { _id: '2', name: 'Test Image 2', path: '/test2.jpg', size: 456 },
+      ],
+      prototype: { save: async () => {} }
+    }
+  : require('../models/images');
 
-var db = []
+router.get('/', async (req, res) => {
+  try {
+    const images = await Image.find();
+    res.render('index', { images, msg: req.query.msg });
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Server Error');
+  }
+});
 
-router.get('/', (req,res)=>{
-    
-    Image.find({}, function(err, images){
-        // console.log(images)
-        if (err) console.log(err);
-        res.render('index',{images:images, msg: req.query.msg })
-    })
-})
+router.post('/upload', (req, res) => {
+  upload(req, res, async (err) => {
+    if (err) return res.redirect(`/?msg=${err}`);
+    if (!req.file) return res.redirect('/?msg=Error: No file selected!');
 
-router.post('/upload', (req, res)=>{
-    upload(req,res, (err)=>{
-        if (err){
-            res.redirect(`/?msg=${err}`);
-        }else{
-            console.log(req.file);
-            // res.send("test");
-            if (req.file == undefined){
-                res.redirect('/?msg=Error: No file selcted!');
-            }else{
-                // const imageObj = {
-                //     id: uuid.v4(),
-                //     name: req.file.filename,
-                //     path: 'images/' + req.file.filename
-                // }
-                // db.push(imageObj);
-                // console.log(db);
-
-                // create new image
-                let newImage = new Image({
-                    name: req.file.filename,
-                    size: req.file.size,
-                    path: 'images/' + req.file.filename
-                })
-
-                // save the uploaded image to the database
-                newImage.save()
-
-                
-                res.redirect('/?msg=File uploaded successfully');
-            }
-        }
-    })
-})
+    try {
+      const newImage = new Image({
+        name: req.file.filename,
+        size: req.file.size,
+        path: 'images/' + req.file.filename,
+      });
+      await newImage.save();
+      res.redirect('/?msg=File uploaded successfully');
+    } catch (err) {
+      console.error(err);
+      res.redirect('/?msg=Error saving file!');
+    }
+  });
+});
 
 module.exports = router;
