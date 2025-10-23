99c319eef906573fcd3146c11adc50a667930c58
Vicmalash <malangavic@yahoo.com>
2025-10-24 00:17:09 +0300

    added credentials to configs

--- _config.js
+++ _config.js
@@ -1,9 +1,13 @@
-var config = {}
+require('dotenv').config({ quiet: true, inject: {} });
 
-// Update to have your correct username and password
+var config = {};
+
+const { MONGO_USERNAME: username, MONGO_PASSWORD: password, MONGO_CLUSTER: cluster, APP_VALUE: app_value,MONGO_DB: prod, MONGO_DB_DEV: dev, MONGO_DB_TEST:test} = process.env;
+
 config.mongoURI = {
-    production: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom?retryWrites=true&w=majority',
-    development: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
-    test: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom-test?retryWrites=true&w=majority',
-}
+    production: `mongodb+srv://${username}:${password}@${cluster}/${prod}?retryWrites=true&w=majority&appName=${app_value}`,
+    development: `mongodb+srv://${username}:${password}@${cluster}/${dev}?retryWrites=true&w=majority&appName=${app_value}`,
+    test: `mongodb+srv://${username}:${password}@${cluster}/${test}?retryWrites=true&w=majority&appName=${app_value}`,
+
+    }
 module.exports = config;
